fee56f2095ea1a20fd2d40fb2345c9a87ae7ebc3
romaingazielly <ganondorf_00@hotmail.fr>
2013-04-14 13:26:03 +0200

    v1
    Création d'une V1 avec fonctionnement JS de la partie works

--- index.php
+++ index.php
@@ -16,7 +16,67 @@
 <link rel="stylesheet" type="text/css" href="css/reset.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <script src="js/jquery.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
 </head>
 <body>
+	<header>
+		<nav>
+			<ul id="menu">
+				<li><a href="#" id="logo"></a></li>
+				<li><a href="#">Home</a></li>
+				<li><a href="#works">Works</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+			<ul>
+				<li><a href="#" id="nav-fb"></a></li>
+				<li><a href="#" id="nav-twitter"></a></li>
+				<li><a href="#" id="nav-linkedin"></a></li>
+				<li><a href="#" id="nav-viadeo"></a></li>
+			</ul>
+		</nav>
+		<aside>
+			<p><span>HI !</span> I'm Romain GAZIELLY, a French web developer looking for some Freelance projects.<a href="#">8</a></p>
+		</aside>
+	</header>
+
+	<div id="container">
+		<section id="works">
+			<div class="button-back"><img src="pics/assets/button_goback.png" border="0" alt="Go back" /></div>
+			<div class="contentwrapper">
+				<div class="works-detail">
+					<div class="works-detail1 works-detail-content">
+						<h1>Mon 1er Projet qui rox du poney</h1>
+						<p>Hello World</p>
+					</div>
+					<div class="works-detail2 works-detail-content">
+						<h1>Coucou</h1>
+						<p>Aloo du shampoing quoi !!!!!!</p>
+					</div>
+				</div>
+
+				<div class="mini-works mini-works1">
+					<h3>Projet 1</h3>
+					<div class="description">Description 1</div>
+					<div class="list">
+						<ul>
+							<li>Nom du projet</li>
+						</ul>
+					</div>
+				</div>
+				<div class="mini-works mini-works2">
+					<h3>Projet 2</h3>
+					<div class="description">Descritpion 2</div>
+					<div class="list">
+						<ul>
+							<li>Nom du projet</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+	<script src="js/jquery.easing.js"></script>
+	<script src="js/jquery.scrollTo.min.js"></script>
 </body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /*----------------------------------
 *							
 *	Version: 1.0				  
-*	Date: 11/02/2013			  
+*	Date: 30/03/2013			  
 *	Author: Romain GAZIELLY	  
 *	Mail: romain.gazielly@gmail.com
 *	Website: www.romaingazielly.com
@@ -11,3 +11,75 @@
 /*-------------------
 	Code
 -------------------*/
+var $window = $(window),
+	$windowHeight = $window.height(),
+	$windowWidth = $window.width(),
+	$worksIsOpen = false;
+
+$(function() {
+	loading();
+	positionWorks();
+
+	$(".mini-works1").click(function() { openServicePage(1); });
+	$(".mini-works2").click(function() { openServicePage(2); });
+
+
+	$(".button-back").click(function() {
+		toggleBackButton();
+		$worksIsOpen = false;
+		for(var i = 6; i > 0; i--) {
+	   		$(".mini-works" + i).animate({left: 0}, {queue: false, duration:700, easing:"easeInOutQuart"});
+	  	}
+	  	$(".works-detail").animate({left: $windowWidth}, {queue: false, duration:700, easing:"easeInOutQuart"});
+	});
+
+	$('nav ul#menu li a').on('click', function(e) {
+		e.preventDefault();
+		$this = $(this);
+		
+		page = $this.attr('href');
+		$.scrollTo( page, 800 );
+	});
+
+});
+
+function openServicePage(index) {
+	$worksIsOpen = true;
+	toggleBackButton();
+	
+	for(var i = 2; i > 0; i--) {
+		$(".mini-works" + i).animate({left: -$windowWidth}, {queue: false, duration:700, easing:"easeInOutQuart"});
+	}
+	$(".works-detail").css("left", $windowWidth);
+
+	$(".works-detail-content").css("opacity", 0).hide();
+	
+	$(".works-detail" + index).show().css("opacity", 1);
+	$(".works-detail").animate({left: 0}, {queue: false, duration:700, easing:"easeInOutQuart"});
+}
+
+function loading() {
+	$('aside').animate({ 'width':'100%' }, 2000, function(){
+		$('aside p').animate({ opacity:1 }, 800);
+		$('header nav').animate({ opacity:1 }, 800);
+		$('#container').animate({ opacity:1}, 800);
+	});
+}
+
+function positionWorks() {
+	if($worksIsOpen == true) {
+		$(".mini-works").css("left", -$windowWidth + "px");
+		$(".works-detail").css("left", 0);
+	} else {
+		$(".mini-works").css("left", "0px");
+		$(".works-detail").css("left", -$windowWidth + "px");
+	}
+}
+
+function toggleBackButton() {
+	if($(".button-back").css("marginLeft") == "-90px") {
+		$(".button-back").stop().delay(1100).animate({marginLeft:0}, {queue: true, duration:300, easing:"easeOutQuad"});
+	} else {
+		$(".button-back").stop().animate({marginLeft:-90}, {queue: false, duration:300, easing:"easeOutQuad"});
+	}
+}
